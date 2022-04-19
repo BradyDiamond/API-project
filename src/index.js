c03909723621b4f2467052ce944eb3fbfcec3a8b
Brady Diamond <brdydmnd@gmail.com>
2022-04-19 09:12:34 -0700

    refactor js to add static methods

--- src/index.js
+++ src/index.js
@@ -2,37 +2,47 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import WeatherService from './weather-service.js'
+
+function clearFields() {
+  $('#location').val("");
+  $('.showErrors').text("");
+  $('.showHumidity').text("");
+  $('.showTemp').text("");
+}
 
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
-    const city = $('#location').val();
-    $('#location').val("");
-
-    let request = new XMLHttpRequest();
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=47b6e4f6da18dfaeecc5a984e1fc78c6
-    `;
-    // let error = new Error;
-
-
-    request.onreadystatechange = function() {
-      if (this.readyState === 4 && this.status === 200) {
-        const response = JSON.parse(this.responseText);
-        getElements(response);
-      }
-      
-    };
-
-    request.open("GET", url, true);
-    request.send();
-   
-  
-      function getElements(response) {
+    let city = $('#location').val();
+    clearFields();
+    let promise = WeatherService.getWeather(city);
+    promise.then(function(response) {
+      const body = JSON.parse(response);
       let farTemp = ((response.main.temp - 273.15) * 9/5 + 32).toFixed(2);
-      $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+      $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
       $('.showTemp').text(`The temperature in fahrenheit is ${farTemp} degrees.`);
-    
-    }
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error}`);
+    });
   });
 });
+//     request.onreadystatechange = function() {
+//       if (this.readyState === 4 && this.status === 200) {
+//         const response = JSON.parse(this.responseText);
+//         getElements(response);
+//         console.log(this.status)
+//       }
+//     }; 
+//       request.open("GET", url, true);
+//       request.send();
+   
+//       function getElements(response) {
+//       let farTemp = ((response.main.temp - 273.15) * 9/5 + 32).toFixed(2);
+//       $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+//       $('.showTemp').text(`The temperature in fahrenheit is ${farTemp} degrees.`);
+    
+//     }
+//   });
+// });
 
 
---
+++ weather-service.js
@@ -0,0 +1,17 @@
+export default class WeatherService {  
+  static getWeather(city) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=47b6e4f6da18dfaeecc5a984e1fc78c6`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(request.response);
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
